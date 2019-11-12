16c779ab7cd14bdefa56a0eca37195bccbe3df6b
JesusMarinGarcia <jesus1998mg@uma.es>
2019-11-12 19:27:24 +0100

    first commit

---
+++ src/test/java/es/uma/ingweb/coffeecar/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.ingweb.coffeecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
