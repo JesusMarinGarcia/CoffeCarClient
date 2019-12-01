3cae94f5f6ebca1fd6e0c13f9412af9a52b2e28a
JesusMarinGarcia <jesus1998mg@uma.es>
2019-12-02 00:09:28 +0100

    Update AnnounceControler.java

--- src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceControler.java
+++ src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceControler.java
@@ -3,12 +3,15 @@ package es.uma.ingweb.coffeecar.controller;
 
 import es.uma.ingweb.coffeecar.consumers.AnnouncementConsumer;
 import es.uma.ingweb.coffeecar.entities.Announcement;
+import es.uma.ingweb.coffeecar.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 
 @Controller
@@ -16,17 +19,18 @@ public class AnnounceControler {
 
     @PostMapping("/announce/create")
     public String announce(
-            @RequestParam(name = "title") String title,
-            @RequestParam(name = "asientos") Integer seats,
-            @RequestParam(name = "link") String link,
+            @RequestParam(name = "title", required = true) String title,
+            @RequestParam(name = "asientos", required = true) Integer seats,
+            @RequestParam(name = "link", required = true) String link,
             @RequestParam(name = "descripcion") String desc,
-            @RequestParam(name = "latOrigen") String latDeparture,
-            @RequestParam(name = "longOrigen") String longDeparture,
-            @RequestParam(name = "latDestino") String latArrival,
-            @RequestParam(name = "longDestino") String longArrival,
-            @RequestParam(name = "arrival") String arrival,
-            @RequestParam(name = "fechaSalida") LocalDateTime departureTime,
-            @RequestParam(name = "fechaLlegada") LocalDateTime arrivalTime
+            @RequestParam(name = "latOrigen", required = true) String latDeparture,
+            @RequestParam(name = "longOrigen", required = true) String longDeparture,
+            @RequestParam(name = "latDestino", required = true) String latArrival,
+            @RequestParam(name = "longDestino", required = true) String longArrival,
+            @RequestParam(name = "arrival", required = true) String arrival,
+            @RequestParam(name = "fechaSalida", required = true) LocalDateTime departureTime,
+            @RequestParam(name = "fechaLlegada", required = true) LocalDateTime arrivalTime,
+            @SessionAttribute("user") User user
             ){
         AnnouncementConsumer announcementConsumer = new AnnouncementConsumer();
         Announcement announcement = new Announcement();
@@ -34,13 +38,19 @@ public class AnnounceControler {
         announcement.setTitle(title);
         announcement.setSeats(seats);
         announcement.setImgLink(link);
-        announcement.setDescription(desc);
+        if (desc.isEmpty() || desc == null){
+            announcement.setDescription("No hay descripción");
+        }else{
+            announcement.setDescription(desc);
+        }
         announcement.setDepartureLatitude(Double.parseDouble(latDeparture));
         announcement.setGetDepartureLongitude(Double.parseDouble(longDeparture));
         announcement.setArrivalLatitude(Double.parseDouble(latArrival));
         announcement.setArrivalLongitude(Double.parseDouble(longArrival));
         announcement.setDepartureTime(departureTime);
         announcement.setArrivalDate(arrivalTime);
+        announcement.setDriver(user);
+        announcement.setPassengers(new ArrayList<>());
         announcementConsumer.create(announcement);
         return "home";
     }
