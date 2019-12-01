bae79c825df5cad173263ac5941943f52b500c0f
Dinohaurio <kilojulio@gmail.com>
2019-12-01 19:23:43 +0100

    Update AnnounceControler.java

--- src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceControler.java
+++ src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceControler.java
@@ -4,6 +4,7 @@ package es.uma.ingweb.coffeecar.controller;
 import es.uma.ingweb.coffeecar.consumers.AnnouncementConsumer;
 import es.uma.ingweb.coffeecar.entities.Announcement;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
