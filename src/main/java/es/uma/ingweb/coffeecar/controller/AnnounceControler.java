7c9d728122da873ab009a01e4cdbbff785e82428
JesusMarinGarcia <jesus1998mg@uma.es>
2019-12-05 18:13:22 +0100

    commit antes de realizar un cambio completo
    aun no funciona

--- src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceControler.java
+++ src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceControler.java
@@ -2,13 +2,14 @@ package es.uma.ingweb.coffeecar.controller;
 
 
 import es.uma.ingweb.coffeecar.consumers.AnnouncementConsumer;
+import es.uma.ingweb.coffeecar.consumers.UserConsumer;
 import es.uma.ingweb.coffeecar.entities.Announcement;
 import es.uma.ingweb.coffeecar.entities.User;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -17,22 +18,29 @@ import java.util.ArrayList;
 @Controller
 public class AnnounceControler {
 
+    private final AnnouncementConsumer announcementConsumer;
+    private final UserConsumer userConsumer;
+
+    public AnnounceControler(AnnouncementConsumer announcementConsumer, UserConsumer userConsumer) {
+        this.announcementConsumer = announcementConsumer;
+        this.userConsumer = userConsumer;
+    }
+
     @PostMapping("createAnnouncement/confirm")
     public String announce(
-            @RequestParam(name = "title", required = true) String title,
-            @RequestParam(name = "asientos", required = true) Integer seats,
-            @RequestParam(name = "link", required = true) String link,
-            @RequestParam(name = "descripcion") String desc,
-            @RequestParam(name = "latOrigen", required = true) String latDeparture,
-            @RequestParam(name = "longOrigen", required = true) String longDeparture,
-            @RequestParam(name = "latDestino", required = true) String latArrival,
-            @RequestParam(name = "longDestino", required = true) String longArrival,
-            @RequestParam(name = "arrival", required = true) String arrival,
-            @RequestParam(name = "fechaSalida", required = true) LocalDateTime departureTime,
-            @RequestParam(name = "fechaLlegada", required = true) LocalDateTime arrivalTime,
-            @SessionAttribute("user") User user
+            @RequestParam(name = "title", required = false) String title,
+            @RequestParam(name = "asientos", required = false) Integer seats,
+            @RequestParam(name = "link", required = false) String link,
+            @RequestParam(name = "descripcion", required = false) String desc,
+            @RequestParam(name = "latOrigen", required = false) String latDeparture,
+            @RequestParam(name = "longOrigen", required = false) String longDeparture,
+            @RequestParam(name = "latDestino", required = false) String latArrival,
+            @RequestParam(name = "longDestino", required = false) String longArrival,
+            @RequestParam(name = "arrival", required = false) String arrival,
+            @RequestParam(name = "fechaSalida", required = false) LocalDateTime departureTime,
+            @RequestParam(name = "fechaLlegada", required = false) LocalDateTime arrivalTime,
+            OAuth2AuthenticationToken authenticationToken
             ){
-        AnnouncementConsumer announcementConsumer = new AnnouncementConsumer();
         Announcement announcement = new Announcement();
         announcement.setArrival(arrival);
         announcement.setTitle(title);
@@ -49,10 +57,10 @@ public class AnnounceControler {
         announcement.setArrivalLongitude(Double.parseDouble(longArrival));
         announcement.setDepartureTime(departureTime);
         announcement.setArrivalDate(arrivalTime);
-        announcement.setDriver(user);
+        announcement.setDriver(userConsumer.getByEmail(authenticationToken.getPrincipal().getAttribute("email")));
         announcement.setPassengers(new ArrayList<>());
         announcementConsumer.create(announcement);
-        return "";
+        return "/home";
     }
 
     @GetMapping("/createAnnouncement")
