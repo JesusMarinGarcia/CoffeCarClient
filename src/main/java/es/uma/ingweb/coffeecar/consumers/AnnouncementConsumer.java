4e6644c86d3edfa11269603d3c62e9db932933c8
Guisanpea <guisanpea@gmail.com>
2019-12-07 15:09:35 +0100

    Added rest controller proxy to handle optionals

--- src/main/java/es/uma/ingweb/coffeecar/consumers/AnnouncementConsumer.java
+++ src/main/java/es/uma/ingweb/coffeecar/consumers/AnnouncementConsumer.java
@@ -19,10 +19,9 @@ import static java.util.stream.Collectors.toList;
 
 @Service
 public class AnnouncementConsumer {
-    private static final String URL ="http://localhost:8080/announced";
-    private static final String GET_ALL_ANNOUNCEMENTS_URL = "http://localhost:8080/announced/search/findAll";
+    private static final String URL = "http://localhost:8080/announced";
     private static final String GET_ANNOUNCEMENTS_BY_DRIVER_URL = "http://localhost:8080/announced/search/findAnnouncesByDriver?user={user}";
-    private static final String  GET_ANNOUNCEMENTS_BY_PASSENGER_URL = "http://localhost:8080/announced/search/findAnnouncesByPassengers?user={user}";
+    private static final String GET_ANNOUNCEMENTS_BY_PASSENGER_URL = "http://localhost:8080/announced/search/findAnnouncesByPassengers?user={user}";
     private static final String GET_ANNOUNCEMENTS_BY_ARRIVAL_DATE_URL = "http://localhost:8080/announced/search/findAnnouncesByArrivalDate?arrivalDate={arrivalDate}";
     private static final String GET_ANNOUNCEMENTS_BY_ARRIVAL_URL = "http://localhost:8080/announced/search/findAnnouncesByArrival?arrival={arrival}";
 
@@ -34,20 +33,28 @@ public class AnnouncementConsumer {
         this.restTemplateProxy = restTemplateProxy;
     }
 
-    public List<Announcement> getAll(){
+    public List<Announcement> getAll() {
         final ResponseEntity<PagedModel<Announcement>> announcementResponse =
-                restTemplate.exchange(
-                        URL,
-                        HttpMethod.GET,
-                        null,
-                        getParameterizedTypeReference()
-                );
+              restTemplate.exchange(
+                    URL,
+                    HttpMethod.GET,
+                    null,
+                    getParameterizedTypeReference()
+              );
         return new ArrayList<>(Objects.requireNonNull(announcementResponse.getBody()).getContent());
     }
-    public List<Announcement> getAvailableAnnouncements(String email){
-        List<Announcement> allTrips = getAll();
-        allTrips.removeAll(getMyTrips(email));
-        return sortByDepartureDate(allTrips);
+
+    public List<Announcement> getAvailableAnnouncements(String email) {
+        return restTemplateProxy.exchange(
+              URL + "/findAvailableAnnounces?email={email}",
+              HttpMethod.GET,
+              null,
+              getParameterizedTypeReference(),
+              email
+        )
+              .map(HttpEntity::getBody).map(CollectionModel::getContent)
+              .map(Collection::stream).map(content -> content.collect(toList()))
+              .orElse(Collections.emptyList());
     }
 
     public List<Announcement> getByDriver(User user) {
@@ -74,51 +81,62 @@ public class AnnouncementConsumer {
                     .map(Collection::stream).map(content -> content.collect(toList()))
                     .orElse(Collections.emptyList());
     }
-    public List<Announcement> getMyTrips(User user){
-        List<Announcement> allMyTrips = getByDriver(user);
-        allMyTrips.addAll(getByPassenger(user));
-        return sortByDepartureDate(allMyTrips);
+
+    public List<Announcement> getMyTrips(String email) {
+        return restTemplateProxy.exchange(
+              URL + "/findUserTrips?email=email",
+              HttpMethod.GET,
+              null,
+              getParameterizedTypeReference(),
+              email)
+              .map(HttpEntity::getBody).map(CollectionModel::getContent)
+              .map(Collection::stream).map(content -> content.collect(toList()))
+              .orElse(Collections.emptyList());
     }
 
-    private List<Announcement> sortByDepartureDate(List<Announcement> announcementList){
+    private List<Announcement> sortByDepartureDate(List<Announcement> announcementList) {
         announcementList.sort(Comparator.comparing(Announcement::getDepartureTime));
         return announcementList;
     }
 
-    public List<Announcement> getByArrivalDate(LocalDateTime arrivalDate){
+    public List<Announcement> getByArrivalDate(LocalDateTime arrivalDate) {
         final ResponseEntity<PagedModel<Announcement>> announcementResponse =
-                restTemplate.exchange(
-                        GET_ANNOUNCEMENTS_BY_ARRIVAL_DATE_URL,
-                        HttpMethod.GET,
-                        null,
-                        getParameterizedTypeReference(),
-                        arrivalDate
-                );
+              restTemplate.exchange(
+                    GET_ANNOUNCEMENTS_BY_ARRIVAL_DATE_URL,
+                    HttpMethod.GET,
+                    null,
+                    getParameterizedTypeReference(),
+                    arrivalDate
+              );
         return new ArrayList<>(Objects.requireNonNull(announcementResponse.getBody()).getContent());
     }
-    public List<Announcement> getByArrival(String arrival){
+
+    public List<Announcement> getByArrival(String arrival) {
         final ResponseEntity<PagedModel<Announcement>> announcementResponse =
-                restTemplate.exchange(
-                        GET_ANNOUNCEMENTS_BY_ARRIVAL_URL,
-                        HttpMethod.GET,
-                        null,
-                        getParameterizedTypeReference(),
-                        arrival
-                );
+              restTemplate.exchange(
+                    GET_ANNOUNCEMENTS_BY_ARRIVAL_URL,
+                    HttpMethod.GET,
+                    null,
+                    getParameterizedTypeReference(),
+                    arrival
+              );
         return new ArrayList<>(Objects.requireNonNull(announcementResponse.getBody()).getContent());
     }
 
     public void create(Announcement announcement) {
         restTemplate.postForEntity(URL, announcement, Announcement.class);
     }
+
     public void delete(Announcement announcement) {
         restTemplate.delete(URL, announcement, Announcement.class);
     }
-    public void edit(Announcement announcement){
+
+    public void edit(Announcement announcement) {
         restTemplate.put(URL, announcement, Announcement.class);
     }
 
     private static ParameterizedTypeReference<PagedModel<Announcement>> getParameterizedTypeReference() {
-        return new ParameterizedTypeReference<>() {};
+        return new ParameterizedTypeReference<>() {
+        };
     }
 }
