3cefe575357ed2bebc31345695bcc61edda25979
JesusMarinGarcia <jesus1998mg@uma.es>
2019-12-08 00:40:31 +0100

    Revert "Merge remote-tracking branch 'origin/Funcionalidades-Inicio' into verAnuncio"
    This reverts commit b01e991256711b54b2daec48555b09552259b61a, reversing
    changes made to bd5c17ee4736f89ed945c2ec8c69336695d41160.

--- src/main/java/es/uma/ingweb/coffeecar/consumers/UserConsumer.java
+++ src/main/java/es/uma/ingweb/coffeecar/consumers/UserConsumer.java
@@ -1,24 +1,19 @@
 package es.uma.ingweb.coffeecar.consumers;
 
+import es.uma.ingweb.coffeecar.RestTemplateProxy;
 import es.uma.ingweb.coffeecar.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.Map;
-import java.util.Objects;
-
 
 @Service
 public class UserConsumer {
@@ -28,14 +23,11 @@ public class UserConsumer {
 
     private final RestTemplate restTemplate;
     private final RestTemplateProxy restTemplateProxy;
-    public UserConsumer(RestTemplate restTemplate, AnnouncementConsumer announcementConsumer, RestTemplateProxy restTemplateProxy) {
-        this.restTemplate = restTemplate;
-        this.announcementConsumer = announcementConsumer;
-        this.restTemplateProxy = restTemplateProxy;
-    }
 
-    public UserConsumer(RestTemplate restTemplate) {
+
+    public UserConsumer(RestTemplate restTemplate, RestTemplateProxy restTemplateProxy) {
         this.restTemplate = restTemplate;
+        this.restTemplateProxy = restTemplateProxy;
     }
 
     public List<User> getAll() {
@@ -49,29 +41,30 @@ public class UserConsumer {
     public Optional<User> optionalGetByEmail(String email) {
         return restTemplateProxy.getForEntity(GET_USER_BY_EMAIL_URL, User.class, email)
               .map(HttpEntity::getBody).orElseThrow(NullPointerException::new);
+    }*/
+
+    public User getByEmail(String email) {
+        return restTemplateProxy.getForEntity(
+              GET_USER_BY_EMAIL_URL,
+              User.class,
+              email).getBody();
     }
 
-    public User getByEmail(String email){
-        ResponseEntity<User> user = restTemplate
-                .getForEntity(
-                        GET_USER_BY_EMAIL_URL,
-                        User.class,
-                        email
-                );
-        return Objects.requireNonNull(user.getBody());
+    public void create(User user) {
+
+        restTemplateProxy.exchange(URL, HttpMethod.POST, new HttpEntity<>(user), getParameterizedTypeReference());
     }
 
-    public void create(User user){
-        restTemplate.postForEntity(URL, user, User.class);
+    public void edit(User user) {
+        restTemplateProxy.exchange(URL, HttpMethod.PUT, new HttpEntity<>(user), getParameterizedTypeReference());
     }
-    public void edit(User user){
-        restTemplate.put(URL, user, User.class);
-    }
-    public void delete(User user){
-        restTemplate.delete(URL, user, User.class);
+
+    public void delete(User user) {
+        restTemplateProxy.exchange(URL, HttpMethod.DELETE, new HttpEntity<>(user), getParameterizedTypeReference());
     }
 
     private static ParameterizedTypeReference<PagedModel<User>> getParameterizedTypeReference() {
-        return new ParameterizedTypeReference<>() {};
+        return new ParameterizedTypeReference<>() {
+        };
     }
 }
--- src/main/java/es/uma/ingweb/coffeecar/controller/HomePageController.java
+++ src/main/java/es/uma/ingweb/coffeecar/controller/HomePageController.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Objects;
 
 @Controller
 public class HomePageController {
@@ -24,30 +25,13 @@ public class HomePageController {
     public String home(OAuth2AuthenticationToken authenticationToken, Model model) {
         String email = authenticationToken.getPrincipal().getAttribute("email");
         String name = authenticationToken.getPrincipal().getAttribute("name");
-        User user = userConsumer.getByEmail(email);
-        if(user==null || user.getEmail()==null)
-            user = createUser(email,name);
-
+        User user = userConsumer.optionalGetByEmail(email)
+              .filter(u -> Objects.nonNull(u.getEmail()))
+              .orElseGet(() -> createUser(email, name));
         model.addAttribute("availableAnnouncements", announcementConsumer.getAvailableAnnouncements(user));
         model.addAttribute("myTrips", announcementConsumer.getMyTrips(user));
 
         return "home";
-    }
-
-    @GetMapping("/")
-    public String availableAnnouncements(OAuth2AuthenticationToken authenticationToken, Model model){
-        model.addAttribute("announcements", announcementConsumer.getAvailableAnnouncements(authenticationToken
-                .getPrincipal()
-                .getAttribute("email")));
-        return "announcementsTable :: resultAnnouncements";
-    }
-
-    @GetMapping("/myTrips")
-    public String showMyTrips(OAuth2AuthenticationToken authenticationToken, Model model){
-        model.addAttribute("announcements", announcementConsumer.getMyTrips(authenticationToken
-                                                                                    .getPrincipal()
-                                                                                    .getAttribute("email")));
-        return "announcementsTable :: resultAnnouncements";
     }
 
     private User createUser(String email, String name) {
