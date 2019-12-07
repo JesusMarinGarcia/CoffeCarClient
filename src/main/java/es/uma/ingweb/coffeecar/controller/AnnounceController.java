d0038f77c5e2a20e8f6c26ccd9cd1d73abb7cf92
JesusMarinGarcia <jesus1998mg@uma.es>
2019-12-07 23:58:47 +0100

    commit antes de probar verAnuncio
    guardar para poder mergear otras ramas

--- src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceController.java
+++ src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceController.java
@@ -2,8 +2,11 @@ package es.uma.ingweb.coffeecar.controller;
 
 
 import es.uma.ingweb.coffeecar.consumers.AnnouncementConsumer;
+import es.uma.ingweb.coffeecar.consumers.BusConsumer;
+import es.uma.ingweb.coffeecar.consumers.StopConsumer;
 import es.uma.ingweb.coffeecar.consumers.UserConsumer;
 import es.uma.ingweb.coffeecar.entities.Announcement;
+import es.uma.ingweb.coffeecar.entities.BusStop;
 import es.uma.ingweb.coffeecar.entities.User;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -17,6 +20,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -63,9 +67,44 @@ public class AnnounceController {
     }
 
     @GetMapping("/announcementDetails")
-    public String announcementDetails(@RequestParam(name="announcementId") long id, Model model){
-        AnnouncementConsumer announcementConsumer = new AnnouncementConsumer();
-        model.addAttribute("announcement",announcementConsumer.getById(id));
+    public String announcementDetails(
+            @RequestParam(name="announcementId") long id,
+            Model model,
+            OAuth2AuthenticationToken authenticationToken){
+        Announcement announcement = announcementConsumer.getById(id);
+        StopConsumer stopConsumer = new StopConsumer();
+        List<BusStop> stops = stopConsumer
+                .getNearby((float)announcement.getDepartureLatitude(),(float)announcement.getGetDepartureLongitude());
+        boolean isDriver = announcement.getDriver()
+                .equals(userConsumer.getByEmail(authenticationToken.getPrincipal().getAttribute("email")));
+        model.addAttribute("isDriver", isDriver);
+        model.addAttribute("announcement",announcement);
+        model.addAttribute("paradas",stops);
         return "announcementDetails";
+    }
+
+    @GetMapping("/announcementEdit")
+    public String announcementEdit(
+            @ModelAttribute Announcement announcement,
+            OAuth2AuthenticationToken authenticationToken){
+        return "";
+    }
+
+    @GetMapping("/announcementDelete")
+    public String announcementDelete(
+            @ModelAttribute Announcement announcement,
+            OAuth2AuthenticationToken authenticationToken,
+            RedirectAttributes redirectAttrs){
+        User driver =  userConsumer.getByEmail(authenticationToken.getPrincipal().getAttribute("email"));
+        if(announcement.getDriver().equals(driver)){
+            announcementConsumer.delete(announcement);
+            redirectAttrs
+                    .addFlashAttribute("mensaje", "Eliminado correctamente");
+            return "redirect:/";
+        }else{
+            redirectAttrs
+                    .addFlashAttribute("mensaje", "No tienes permiso para esta acción");
+            return "redirect:/";
+        }
     }
 }
