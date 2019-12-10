6073012309ff2c21405ea1414182a9a01318303b
JavierC07 <36677101+JavierC07@users.noreply.github.com>
2019-12-10 21:24:34 +0100

    OTRO

--- src/main/java/es/uma/ingweb/coffeecar/consumers/AnnouncementConsumer.java
+++ src/main/java/es/uma/ingweb/coffeecar/consumers/AnnouncementConsumer.java
@@ -1,8 +1,6 @@
 package es.uma.ingweb.coffeecar.consumers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.sun.nio.sctp.AbstractNotificationHandler;
-import es.uma.ingweb.coffeecar.RestTemplateProxy;
 import es.uma.ingweb.coffeecar.entities.Announce;
 import es.uma.ingweb.coffeecar.entities.User;
 import org.springframework.core.ParameterizedTypeReference;
@@ -11,18 +9,17 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.client.Traverson;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
-import static java.util.stream.Collectors.toList;
 import static org.springframework.hateoas.MediaTypes.HAL_JSON;
 
 @Service
@@ -112,10 +109,13 @@ public class AnnouncementConsumer {
 
     public void delete(Announce announce) {
         restTemplate.delete(announce.getSelfURI());
+
     }
 
     public void edit(Announce announce) {
-        restTemplate.put(URL, announce, Announce.class);
+
+        restTemplate.put(announce.getLink("self").map(Link::getHref).get(), Announce.class);
+
     }
 
     private static ParameterizedTypeReference<PagedModel<Announce>> getParameterizedTypeReference() {
--- src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceController.java
+++ src/main/java/es/uma/ingweb/coffeecar/controller/AnnounceController.java
@@ -13,10 +13,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -27,10 +24,12 @@ public class AnnounceController {
 
     private final AnnouncementConsumer announcementConsumer;
     private final UserConsumer userConsumer;
+    private final StopConsumer stopConsumer;
 
-    public AnnounceController(AnnouncementConsumer announcementConsumer, UserConsumer userConsumer) {
+    public AnnounceController(AnnouncementConsumer announcementConsumer, UserConsumer userConsumer, StopConsumer stopConsumer) {
         this.announcementConsumer = announcementConsumer;
         this.userConsumer = userConsumer;
+        this.stopConsumer = stopConsumer;
     }
 
     @PostMapping("createAnnouncement/confirm")
@@ -67,9 +66,8 @@ public class AnnounceController {
             Model model,
             OAuth2AuthenticationToken authenticationToken){
         Announce announcement = announcementConsumer.getAnnouncementByURI(uri);
-        StopConsumer stopConsumer = new StopConsumer();
-        List<BusStop> stops = stopConsumer
-                .getNearby((float)announcement.getDepartureLatitude(),(float)announcement.getDepartureLongitude());
+
+        List<BusStop> stops = stopConsumer.getNearby((float)announcement.getDepartureLatitude(),(float)announcement.getDepartureLongitude());
         User user = userConsumer.getByEmail(authenticationToken.getPrincipal().getAttribute("email"));
         boolean isDriver = announcement.getDriver()
                 .equals(user);
@@ -95,6 +93,44 @@ public class AnnounceController {
             redirectAttrs
                     .addFlashAttribute("mensaje", "No tienes permiso para esta acción");
         }
+        return "redirect:/";
+    }
+
+    // metodo al querer editar un anuncio
+    @GetMapping("/editarAnuncio")
+    public String editAnnouncement(@RequestParam(name="announcementURI") String uri,
+                                   Model model){
+            System.out.println(uri);
+            Announce announce = announcementConsumer.getAnnouncementByURI(uri);
+            model.addAttribute("announce", announce);
+            model.addAttribute("uri",uri);
+        return "editAnnouncement";
+    }
+    //metodo cuando se modifica el anuncio
+    @GetMapping("/editarAnuncio/confirm")
+    public String changeAnnouncement(@ModelAttribute Announce announce,@RequestParam("UriAnnounce") String uri, Model model,
+                                     OAuth2AuthenticationToken authenticationToken, RedirectAttributes redirectAttrs){
+        if (announce.getDescription() == null || announce.getDescription().isEmpty()) {
+            announce.setDescription("No hay descripción");
+        }
+        Announce announce1 = announcementConsumer.getAnnouncementByURI(uri);
+        announce.add(announce1.getLinks());
+        announcementConsumer.edit(announce);
+
+
+    /*
+        List<BusStop> stops = stopConsumer
+                .getNearby((float)announcement.getDepartureLatitude(),(float)announcement.getDepartureLongitude());
+        User user = userConsumer.getByEmail(authenticationToken.getPrincipal().getAttribute("email"));
+        boolean isDriver = announcement.getDriver()
+                .equals(user);
+        boolean isPassenger = announcement.getPassengers().contains(user);
+        model.addAttribute("isDriver", isDriver);
+        model.addAttribute("isPassenger", isPassenger);
+        model.addAttribute("announcement", announcement);
+        model.addAttribute("paradas", stops);*/
+        redirectAttrs
+                .addFlashAttribute("mensaje", "Agregado correctamente");
         return "redirect:/";
     }
 }
